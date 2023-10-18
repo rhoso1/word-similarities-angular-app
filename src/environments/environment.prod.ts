eb5c09ca3a5fbaa9682d8574e38e60cbc09a821c
Ramiza Hošo <ramiza.hoso@gmail.com>
2023-10-18 12:40:55 +0200

    Removed extra slash in env ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://rhoso1.github.io/word-similarities-node-app/',
+    apiBaseUrl: 'https://rhoso1.github.io/word-similarities-node-app/api/wordpairs',
   };
