e6529f4801cb03a8f89cb4f6b5d8bf40bb7358b7
Ramiza Hošo <ramiza.hoso@gmail.com>
2023-10-18 12:26:21 +0200

    added files to angular.json for environment prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: "https://rhoso1.github.io/word-similarities-node-app",
-  // apiBaseUrl: 'http://localhost:8080/api/wordpairs',
+  apiBaseUrl: 'http://localhost:8080/api/wordpairs',
 };
